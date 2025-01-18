3c657eb73ae6c82e367398cf943081c7d64b4878
pappukrs <digitalpappu101@gmail.com>
2025-01-18 18:17:58 +0530

    added new comoents in web

---
+++ app/app/notifications/index.tsx
@@ -0,0 +1,14 @@
+import RealTimeNotifications from '@/components/RealTimeNotifications'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      {/* <RealTimeNotifications /> */}
+    </View>
+  )
+}
+
+export default index
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 
 const Header: React.FC = () => {
+  const router = useRouter();
+
+  const handleNotificationPress = () => {
+    router.push({
+      pathname: '/notifications/index'
+    });
+  };
+
   return (
     <View style={styles.headerContainer}>
       
       <View style={styles.iconContainer}>
         <Ionicons name="menu" size={24} color="black" />
-        <Ionicons name="notifications" size={24} color="black" />
+        <TouchableOpacity onPress={handleNotificationPress}>
+          <Ionicons name="notifications" size={24} color="black" />
+        </TouchableOpacity>
       </View>
     </View>
   );
